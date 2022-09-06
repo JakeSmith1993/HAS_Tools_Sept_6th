7141d73d6cfef6be1a1bbaa8626fe2e56ed26d3a
JakeSmith1993 <jsmith77@email.arizona.edu>
2022-09-06 09:13:38 -0700

    updates!!

--- air_pressure.py
+++ air_pressure.py
@@ -1,13 +1,5 @@
 import math
 
-p0 = 101325                         #ref pressure (P)
-L = 0.00976                         #Temp lapse rate
-M = 0.02896968                      #Molar mass of dry air (kg/mol)
-cp = 1004.68506                     #Constant-pressure spec. heat
-g = 9.80665                         #Acceleration due to gravity (m/s^2)
-R0 = 8.314462618                    #Universal gas constant (J/mol*K)
-T = 273                             #Temperature (K)
-
 h_list = range(0, 1000, 10)         #Height (m)
 #h_list = [0,10,100,1000]           #Height (m)
 
